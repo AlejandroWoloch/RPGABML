2542a59fef260118b2da25f566af897074b95914
Carlos Ducamp <ducampcarlos@gmail.com>
2019-06-06 14:25:09 -0300

    Added commentaries explaining the interface and each attribute

--- IAbility.java
+++ IAbility.java
@@ -1,5 +1,5 @@
 
-public interface IAbility {
-	int cost;
-	int levelRequired;
+public interface IAbility { //An interface created to make every ability (Spell or Skill at the moment) be forced to follow certain rules.
+	int cost; //Stamina or Mana that will be consumed once the Ability is used.
+	int levelRequired; //The PlayableCharacter will need to reach certain level to use this ability.
 }
