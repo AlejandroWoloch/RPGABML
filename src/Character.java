e52e6a54e7e32a85278e1d8729bb234242f04c19
Florencia Gonzalez <rightonhana@gmail.com>
2019-06-15 21:22:26 -0300

    Added logic for methods and change some attributes

--- src/Character.java
+++ src/Character.java
@@ -1,15 +1,22 @@
 
 public abstract class Character {
+
 	// Attributes
+	private static int count = 0;
+	private int id;
 	private byte level;
-	private byte maxLevel;
-	private short attackPerLevel;
-	private short defensePerLevel;
-	private short hpPerLevel;
+	private static byte maxLevel = 50;
+	private static short baseAtack = 5;
+	private static short baseDefense = 2;
+	private static short baseHp = 50;
 	private String name;
 	
 	// Constructor
-	public Character() {}
+	public Character(byte level, String name) {
+		this.setId();
+		this.setLevel(level);
+		this.setName(name);
+	}
 
 	// Methods
 	/**
@@ -17,23 +24,38 @@ public abstract class Character {
 	 * 
 	 * This method will be defined and implemented by the child classes
 	 * */
-	public abstract short calculateAttack();
+	public short calculateAttack() {
+		short random = (short)(Math.random() * (1 - 0)); 
+		return (short) (Character.baseAtack  + ( Character.baseAtack * random ) * Character.baseHp);
+	}
 	
 	/**
 	 * Abstract method to calculate the defense of a character
 	 * 
 	 * This method will be defined and implemented by the child classes
 	 * */
-	public abstract short calculateDefense();
+	public short calculateDefense() {
+		return (short) ((Character.baseDefense * this.level) + (this.calculateHp() * 0.05));
+	}
 	
 	/**
 	 * Abstract method to calculate the hp of a character
 	 * 
 	 * This method will be defined and implemented by the child classes
 	 * */
-	public abstract short calculateHp();
+	public short calculateHp() {
+		return (short) ((Character.baseHp * this.level) - (0.5 * this.calculateAttack()));
+	}
 
 	// Getters and Setters
+	public int getId() {
+		return id;
+	}
+
+	public void setId() {
+		this.id = ++Character.count;
+	}
+	
 	public byte getLevel() {
 		return level;
 	}
@@ -47,31 +69,7 @@ public abstract class Character {
 	}
 
 	public void setMaxLevel(byte maxLevel) {
-		this.maxLevel = maxLevel;
-	}
-
-	public short getAttackPerLevel() {
-		return attackPerLevel;
-	}
-
-	public void setAttackPerLevel(short attackPerLevel) {
-		this.attackPerLevel = attackPerLevel;
-	}
-
-	public short getDefensePerLevel() {
-		return defensePerLevel;
-	}
-
-	public void setDefensePerLevel(short defensePerLevel) {
-		this.defensePerLevel = defensePerLevel;
-	}
-
-	public short getHpPerLevel() {
-		return hpPerLevel;
-	}
-
-	public void setHpPerLevel(short hpPerLevel) {
-		this.hpPerLevel = hpPerLevel;
+		Character.maxLevel = maxLevel;
 	}
 
 	public String getName() {
@@ -80,6 +78,38 @@ public abstract class Character {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public static int getCount() {
+		return count;
+	}
+
+	public static void setCount(int count) {
+		Character.count = count;
+	}
+
+	public static short getBaseAtack() {
+		return baseAtack;
+	}
+
+	public static void setBaseAtack(short baseAtack) {
+		Character.baseAtack = baseAtack;
+	}
+
+	public static short getBaseDefense() {
+		return baseDefense;
+	}
+
+	public static void setBaseDefense(short baseDefense) {
+		Character.baseDefense = baseDefense;
+	}
+
+	public static short getBaseHp() {
+		return baseHp;
+	}
+
+	public static void setBaseHp(short baseHp) {
+		Character.baseHp = baseHp;
 	}
 
 }
