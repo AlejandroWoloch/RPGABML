5961e5099eed1dd84b06f1694708e2a5b1e1caf1
Florencia Gonzalez <rightonhana@gmail.com>
2019-06-22 21:07:05 -0300

    Added lasId attribute, validations and override toString and equals methods

--- src/User.java
+++ src/User.java
@@ -1,22 +1,67 @@
+import java.util.Objects;
 
 public abstract class User {
 	// Attributes
+	private static int lastId = 0;
 	private int id;
 	private String name;
 	private String username;
 	private String password;
 	private String email;
+	protected static final String errorMessage = "Error: Invalid value";
 	
 	// Constructor
-	public User() {}
+	public User(String name, String username, String email) {
+		this.setId();
+		this.setName(name);
+		this.setUsername(username);
+		this.setEmail(email);
+	}
+	
+	public User(String name, String username, String password, String email) {
+		this.setId();
+		this.setName(name);
+		this.setUsername(username);
+		this.setPassword(password);
+		this.setEmail(email);
+	}
+	
+	// Override equals method.
+	@Override
+	public boolean equals(Object obj) {
+		if (obj != null && obj.getClass().isInstance(User.class)) {
+			return ((User) obj).getId() == this.getId();
+		} else {
+			return false;
+		}
+	}
+	
+	// Override hashCode method.
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	// Override toString method.
+	@Override
+	public String toString() {
+		return "Character [Id = " + getId() +
+				", Name = " + getName() + 
+				", Username = " + getUsername() + 
+				", Email = " + getEmail() + " ]";
+	}
 
 	// Getters and Setters
+	private static int getLastId() {
+		return ++User.lastId;
+	}
+	
 	public int getId() {
 		return id;
 	}
 
-	public void setId(int id) {
-		this.id = id;
+	private void setId() {
+		this.id = User.getLastId();
 	}
 
 	public String getName() {
@@ -24,7 +69,11 @@ public abstract class User {
 	}
 
 	public void setName(String name) {
-		this.name = name;
+		if(name.length() > 0) {
+			this.name = name;			
+		} else {
+			System.out.println(User.errorMessage + "... Dude, please... Your fathers thought a name for you... Just use it");
+		}
 	}
 
 	public String getUsername() {
@@ -32,7 +81,11 @@ public abstract class User {
 	}
 
 	public void setUsername(String username) {
-		this.username = username;
+		if(username.length() > 0) {			
+			this.username = username;
+		} else {
+			System.out.println(User.errorMessage + "... Dude, please... Create a username, don't be lazy e.e");
+		}
 	}
 
 	public String getPassword() {
@@ -40,7 +93,11 @@ public abstract class User {
 	}
 
 	public void setPassword(String password) {
-		this.password = password;
+		if(username.length() > 0) {
+			this.password = password;
+		} else {
+			System.out.println(User.errorMessage + "... That is not a password. Or maybe yes, if you like or want to have huge security problems");
+		}
 	}
 
 	public String getEmail() {
@@ -48,6 +105,10 @@ public abstract class User {
 	}
 
 	public void setEmail(String email) {
-		this.email = email;
+		if(email.length() > 0) {
+			this.email = email;
+		} else {
+			System.out.println(User.errorMessage + "... That is not even an email");
+		}
 	}	
 }
