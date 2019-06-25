2808e38c26f90f265b69b38829b45151aba052cf
Carlos Ducamp <ducampcarlos@gmail.com>
2019-06-25 06:37:31 -0300

    Added attribute and method

--- src/IAbility.java
+++ src/IAbility.java
@@ -1,4 +1,8 @@
 
-public interface IAbility {
-
+public interface IAbility { //An interface created to make every ability (Spell or Skill at the moment) be forced to follow certain rules.
+	//Attributes
+	int baseCost=3; //Base cost that'll be used to determine the cost of each ability
+	
+	//Methods
+	int calculateFinalCost();
 }
