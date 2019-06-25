a7f1e7f478d4e15b1bc4fdb124ed78bed1e745eb
Florencia Gonzalez <rightonhana@gmail.com>
2019-06-25 19:50:51 -0300

    Created SystemError class

---
+++ src/SystemMessage.java
@@ -0,0 +1,11 @@
+
+public class SystemMessage {
+
+	// Attributes
+	private final String ErrorInputValue = "Error: Invalid value";
+
+	// Getters and Setters
+	public String getErrorInputValue() {
+		return ErrorInputValue;
+	}
+}
