0a112889ded4f059bd3c56417439d0b18f621592
Carlos Ducamp <ducampcarlos@gmail.com>
2019-06-27 17:53:10 -0300

    Defined Constructors, added validation for gold with try/catch, redefined toString, added Equals and Hashcode

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 
 public class Player extends User { 
 	/*
@@ -10,25 +11,36 @@ public class Player extends User {
 	//private ABML<PlayableCharacter team; //If there's time, there'll be a Collections of characters that the Player could use.
 	private int gold; //Amount of gold the Player will have
 	
-	//Constructor
-	public Player() {}
+	//Constructors
+	public Player(String name, String username, String password, String email) {
+		super(name, username, password, email);
+		this.setGold(0);
+		this.team= null;
+	}
+
+	public Player(String name, String username, String email) {
+		super(name, username, email);
+		this.setGold(0);
+		this.team= null;
+	}
+
+	public Player(String name, String username, String password, String email, int gold) {
+		super(name, username, password, email);
+		this.setGold(gold);
+		this.team= null;
+	}
+
+	public Player(String name, String username, String email, int gold) {
+		super(name, username, email);
+		this.setGold(gold);
+		this.team= null;
+	}
 
 	
 	//Getters and Setters
-	/*public ABML<item> getInventory() {
-		return inventory;
-	}*/
-
-	/*public void setInventory(ABML<item> inventory) {
-		this.inventory = inventory;
-	}*/
 
 	public PlayableCharacter getTeam() {
 		return team;
-	}
-
-	public void setTeam(PlayableCharacter team) {
-		this.team = team;
 	}
 
 	public int getGold() {
@@ -36,22 +48,51 @@ public class Player extends User {
 	}
 
 	public void setGold(int gold) {
-		this.gold = gold;
+		try {
+			if(gold>=0) {
+				this.gold=gold;
+			}else {
+				throw new Exception();
+			}
+		}
+		catch (Exception e){
+			System.out.println(errorMessage + " - Gold should be a positive value");
+		}
 	}
 	
 	//Methods
-	public void administrateCharacters() {}
+	public void administrateCharacters() {/*Don't know what objective will have this method*/}
 	
-	public void startGame() {}
+	public void startGame() {/*Will be defined when we have all the other classes defined*/}
 	
-	public void createCharacter() {}
+	public void createCharacter(/*Here goes every detail to define the character*/) {
+		/*
+		 * Here goes the logic to define which Class of playable character is being created
+		 */
+	}
 	
-	public void administrateInventory() {}
+	//public void administrateInventory() {} //Not going to use this for now
 
 
+	//To String, Equals, Hashcode.
+	
 	@Override
 	public String toString() {
-		return "Player";
+		String message= "/nPlayer [Character name: %s, Amount of Gold: %d";
+		return String.format(super.toString() + message, team.getName(), this.getGold());
+	}
+	
+	public boolean equals(Object obj) {
+		if (obj != null && obj.getClass().isInstance(Player.class)) {
+			return ((User) obj).getId() == this.getId();
+		} else {
+			return false;
+		}
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(getId());
 	}
 	
 	
