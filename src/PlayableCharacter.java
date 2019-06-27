055d38d999bb1adf0d22705e156a24fc88e41c55
Woloch Alejandro <ale_woloch@live.com>
2019-06-26 21:23:46 -0300

    Added toString and equals methods, Comented stuff releated to Item class

--- src/PlayableCharacter.java
+++ src/PlayableCharacter.java
@@ -1,28 +1,37 @@
+import java.util.Objects;
 
 public abstract class PlayableCharacter extends Character{
 
 	//Atributes
 	private static byte nextlevelXP = 100; 	//Used for levelingUp
 	private int experience;					//Current XP, can't exceed experience for next level.
-	private Weapon equipedWeapon;
-	private Armor equipedArmor;
+	//private Weapon equipedWeapon;			//Comented until we implement Items class
+	//private Armor equipedArmor;
 	
 	
 	//Constructors
 	public PlayableCharacter(String name) { //For brand new Characters that will start at level 1
-		super(1, name);
-		this.equipedWeapon = null;
-		this.equipedArmor = null;
+		super((byte) 1, name);
+		//this.equipedWeapon = null;	//Comented until we implement Items class
+		//this.equipedArmor = null;
 		this.setExperience(0);
 	}
 	
+	public PlayableCharacter(byte level, String name, int experience) { //Used to instance characters with data from JSON
+		super(level, name);
+		this.setExperience(experience);
+	}
+	
+	/* Replace last constructor with this once we have Items class implemented.
 	public PlayableCharacter(byte level, String name, int experience, Weapon weapon, Armor armor) { //Used to instance characters with data from JSON
 		super(level, name);
 		this.equipWeapon(weapon);
 		this.equipArmos(armor);
 		this.setExperience(experience);
 	}
+	*/
 	
+	/* //Comented until we implement Items class
 	public abstract void equipWeapon(Weapon weapon);
 	
 	public Weapon getWeapon() {
@@ -34,6 +43,7 @@ public abstract class PlayableCharacter extends Character{
 	public Armor getArmor() {
 		return this.equipedArmor;
 	}
+	*/
 	
 	private void setExperience(int experience) {	//I made it private so you are forced to use the gainExperience method
 		if(experience >= 0) {
@@ -50,16 +60,16 @@ public abstract class PlayableCharacter extends Character{
 	}
 	
 	public int getNextLevelXPNeeded() {		//I did a random exponential calculation, not necessarily final
-		return (int) (nextlevelXP * (Math.pow(this.level, 1.5f)));
+		return (int) (nextlevelXP * (Math.pow(this.getLevel(), 1.5f)));
 	}
 	
 	private boolean isLevelingUpAviable() {		//Controls if current Xp exceeds required XP, and if you are not max level already
-		return ((this.experience > this.getNextLevelXPNeeded()) && (this.level < maxLevel));
+		return ((this.experience > this.getNextLevelXPNeeded()) && (this.getLevel() < this.getMaxLevel()));
 	}
 	
 	private void levelUp() {		//Used by setExperience method to levelUp if necessary
 		int newXP = this.getExperience() - this.getNextLevelXPNeeded();
-		this.setLevel(this.getLevel() + 1);
+		this.setLevel((byte) (this.getLevel() + 1));
 		this.setExperience(newXP);
 	}
 	
@@ -67,5 +77,24 @@ public abstract class PlayableCharacter extends Character{
 		if(amount > 0) {
 			this.setExperience(amount + this.getExperience());
 		}
+	}
+	
+	
+	//toString and Equals
+	@Override
+	public boolean equals(Object obj) {
+		return super.equals(obj);
+	}
+	
+	// Override hashCode method.
+	@Override
+	public int hashCode() {
+		return super.hashCode();
+	}
+
+	// Override toString method.
+	@Override
+	public String toString() {
+		return "PlayableCharacter [Current XP = " + this.getExperience() + ", XP for next level = " + this.getNextLevelXPNeeded() + " ]" + super.toString();
 	}
 }
