b88ccab7255dc875bd6586d3fa8b47cefd583297
Woloch Alejandro <ale_woloch@live.com>
2019-06-27 23:08:51 -0300

    Added Generic to Class, Added atribute list, Added list method

--- src/ABML.java
+++ src/ABML.java
@@ -1,4 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
 
-public class ABML {
+public class ABML<T> {
 
+	    List<T> list= new ArrayList<T>(); //Se instancia en el constructor
+	    
+	    void list(){
+	        if(! list.isEmpty()) {
+	        	for (T t : list) {
+					System.out.println(t.toString() + "\n");
+				}
+	        }
+	        else {
+	        	System.out.println("No elements in this list.");
+	        }
+	    }
 }
