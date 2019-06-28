203209a6eac45c1e5fa9e3dc630ca4d2120d42af
Woloch Alejandro <ale_woloch@live.com>
2019-06-27 22:42:46 -0300

    Comented code related to Skills

--- src/PhysicalClass.java
+++ src/PhysicalClass.java
@@ -4,26 +4,38 @@ public abstract class PhysicalClass extends PlayableCharacter{
 	//Atributes
 	private static final int maxStamina = 100;
 	private int stamina;
-	protected ArrayList<PhysicalSkill> skillList;
-	private int maxSkills
+	//protected ArrayList<PhysicalSkill> skillList;  //Comented until we implement Skills
+	private int maxSkills;
+	private static String msgError = "Error: Invalid value.";
 	
 	
 	//Constructors
-	public PhysicalClass(String name) { //For brand new PhysicalCharacters
+	public PhysicalClass(String name) throws Exception{ //For brand new PhysicalCharacters
 		super(name);
 		this.setStamina(maxStamina);
 	}
 	
+	
+	public PhysicalClass(byte level, String name, int experience, int stamina) throws Exception{ //Used to instance characters with data from JSON
+		super(level, name,experience);
+		this.setStamina(stamina);
+	}
+	
+	/* COMENTED UNTIL WE IMPLEMENT ITEMS
 	public PhysicalClass(byte level, String name, int experience, Weapon weapon, Armor armor, int stamina) {  //Used to instance with information from JSON
 		super(level,name,experience,weapon,armor);
 		this.setStamina(stamina);
 	}
-	
+	*/
 	
 	//Getters and Setters
-	public void setStamina(int stamina) {
+	public void setStamina(int stamina) throws Exception{
 		if(stamina >= 0 && stamina <= maxStamina) {
 			this.stamina = stamina;
+		}
+		else {
+			String msg = "Stamina has to be higher than zero and lower than maxStamina";
+			throw new Exception(PhysicalClass.msgError + msg);
 		}
 	}
 	
@@ -35,6 +47,7 @@ public abstract class PhysicalClass extends PlayableCharacter{
 		return maxStamina;
 	}
 	
+	/*COMENTED UNTIL WE IMPLEMENT SKILLS
 	public ArrayList<PhysicalSkill> getSkillList() {
 		return this.skillList;
 	}
@@ -55,4 +68,25 @@ public abstract class PhysicalClass extends PlayableCharacter{
 			this.skillList.remove(toRemove);
 		}
 	}
+	
+	*/
+	
+	
+	//toString and Equals
+		@Override
+		public boolean equals(Object obj) {
+			return super.equals(obj);
+		}
+		
+		// Override hashCode method.
+		@Override
+		public int hashCode() {
+			return super.hashCode();
+		}
+
+		// Override toString method.
+		@Override
+		public String toString() {
+			return "PhysicalClass [Stamina = " + this.getStamina() + "]" + super.toString();
+		}
 }
