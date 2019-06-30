f42c2db2194724359571dbf6c283854d0ac9c450
Florencia Gonzalez <rightonhana@gmail.com>
2019-06-30 17:48:57 -0300

    Added create method to CRUD class

--- src/CRUD.java
+++ src/CRUD.java
@@ -3,8 +3,28 @@ import java.util.List;
 
 public class CRUD<T extends Base> {
 
+		// Attributes
 	    List<T> list = null;
 	    
+	    // Methods
+	    
+	    // CRUD methods
+	    /**
+	     * CREATE method
+	     * Adds an Object in the List
+	     */
+	    List<T> create(T obj) {
+	    	if(obj != null) {
+	    		list.add(obj);
+	        } else {
+	        	throw new Exception("The object does not have to be an empty object");
+	        }
+	    }
+	    
+	    /**
+	     * READ method
+	     * Show the content of the List
+	     */
 	    void read(){
 	        if(!list.isEmpty()) {
 	        	for (T t : list) {
@@ -16,19 +36,37 @@ public class CRUD<T extends Base> {
 	        }
 	    }
 	    
-	    List<T> getList(){
-	    	return list;
-	    }
-	    
-	    void initialize() {
-	    	list= new ArrayList<T>();
-	    }
-	    
+	    /**
+	     * DELETE method
+	     * Delete an Object based on its id
+	     */
 	    void delete(int id) {
 	    	T obj=find(id);
 	    	list.remove(obj);
 	    }
 	    
+	    
+	    // Other methods
+	    /**
+	     * GETLIST method
+	     * returns the List
+	     */
+	    List<T> getList(){
+	    	return list;
+	    }
+	    
+	    /**
+	     * INITIALIZE method
+	     * Initialize an ArrayList: Create a new ArrayList of T Objects
+	     */
+	    void initialize() {
+	    	list= new ArrayList<T>();
+	    }
+	    
+	    /**
+	     * FIND method
+	     * Find an Object based on its id
+	     */
 	    T find(int id) {
 	    	T obj=null;
 	    	if(!list.isEmpty()) {
