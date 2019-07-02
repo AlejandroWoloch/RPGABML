b2accb25e11aa17a01f9374c1ec19f1b3203d1e1
Woloch Alejandro <ale_woloch@live.com>
2019-07-01 21:16:06 -0300

    Created AdminClasss

---
+++ src/Admin.java
@@ -0,0 +1,20 @@
+
+public class Admin extends User{
+
+	public Admin(String name, String username, String email) throws Exception {
+		super(name, username, email);
+	}
+
+	public Admin(String name, String username, String password, String email) throws Exception {
+		super(name, username, password, email);
+	}
+
+	
+	public void administrateMonsters() {
+		
+	}
+	
+	public void administratePlayers() {
+		
+	}
+}
