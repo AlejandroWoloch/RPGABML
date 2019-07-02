d1150bcd29d8ef386dd534217c7aa6fd60cf9350
Woloch Alejandro <ale_woloch@live.com>
2019-07-02 18:57:28 -0300

    Added delete atribute to Base Class

--- src/Base.java
+++ src/Base.java
@@ -1,8 +1,22 @@
 
 public class Base {
+	
 	protected int id;
+	boolean deleted;
+	
+	public Base() {
+		setDeleted(false);
+	}
 	
 	public int getId() {
 		return this.id;
+	}
+	
+	public boolean isDeleted() {
+		return this.deleted;
+	}
+	
+	public void setDeleted(boolean deleted) {
+		this.setDeleted(deleted);
 	}
 }
